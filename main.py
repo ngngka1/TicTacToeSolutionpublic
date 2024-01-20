ab54aba0a644137563cb62ca3c9e43e307db0e44
ngngka1 <ngwewe2015@gmail.com>
2024-01-20 16:09:31 +0800

    added game grid input

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import psutil
 import csv
 import sys
+import curses
 
             
 def backtrack(game: list, players: dict, rounds: int, user: str) -> list:
@@ -58,17 +59,20 @@ def check_checkmated(row_num, col_num, game: list, player): # this return the po
 def attack(target_row, target_col, game, curr_player, opponent): #
     for i, row in enumerate(game):
         for j, element in enumerate(row):
-            if (game[i][j] == "_" 
-                and (
+            if game[i][j] == "_":
+                rowset = set([game[i][j + 1 - len(row)], game[i][j + 2 - len(row)], element])
+                colset = set([game[i + 1 - len(game)][j], game[i + 2 - len(game)][j], element])
+                descending_cross = set([game[i + 1 - len(game)][j + 1 - len(row)], game[i + 2 - len(game)][j + 2 - len(row)], element])
+                ascending_cross = set([game[len(game) - i - 1][j + 1 - len(row)], game[len(game) - i - 2][j + 2 - len(row)], element])
+                if (
                     # approach of checking set len == 2 works because cases like XX_ and OOX are eliminated beforehand
-                    len(set([game[i][j + 1 - len(row)], game[i][j + 2 - len(row)], element])) == 2 # horizontal
-                    or len(set([game[i + 1 - len(game)][j], game[i + 2 - len(game)][j], element])) == 2 # vertical #
-                    or (i == j and len(set([game[i + 1 - len(game)][j + 1 - len(row)], game[i + 2 - len(game)][j + 2 - len(row)], element])) == 2)
-                    or (abs(i-j) == 2 and len(set([game[len(game) - i - 1][j + 1 - len(row)], game[len(game) - i - 2][j + 2 - len(row)], element])) == 2)
-                    )
-                ):
-                if (i, j) == (target_row, target_col):
-                    return True
+                    (opponent not in rowset and len(rowset) == 2) # horizontal
+                    or (opponent not in colset and len(colset) == 2) # vertical
+                    or (i == j and opponent not in descending_cross and len(descending_cross) == 2)
+                    or (abs(i-j) == 2 and opponent not in ascending_cross and len(ascending_cross) == 2)
+                    ):
+                    if (i, j) == (target_row, target_col):
+                        return True
     return False
 
 def valid_move(row_num, col_num, game, players, rounds): # this function returns true if the move is valid(block or attack)
@@ -89,39 +93,93 @@ def valid_move(row_num, col_num, game, players, rounds): # this function returns
         return False
     else:
         return attack(row_num, col_num, game[:][:], players[rounds % 2], players[(rounds + 1) % 2])
-    
-def prompt(game: list, players: dict):
-    print("Game Grid: ")
-    for row in game:
-        print(" ".join(row))
-    print("Player 1: " + players[0])
-    print("Player 2: " + players[1])
+   
+def input_gamegrid(stdscr, game: list, players: dict):
+    stdscr = curses.initscr()
+    stdscr.nodelay(0)
+    row_num = 0
+    col_num = 0
+    rounds = 0
+    game[row_num][col_num] = "{" + game[row_num][col_num] +"}" # selecting cursor
     inputting = True
     while inputting:
-        selection = (input("Are you Player 1 or 2? (1/2)"))
+        stdscr.clear()
+        stdscr.addstr(0, 0, "Current Game Grid: ")
+        for i, row in enumerate(game, 0):
+            stdscr.addstr(1 + i, 0, " ".join(row))
+        stdscr.addstr(2 + i, 0, f"Chess to be placed next: {players[rounds % 2]}")
+        stdscr.refresh()
+        key = stdscr.getch()
+        if key == ord("q"):
+            game[row_num][col_num] = game[row_num][col_num][1]
+            break
+        elif key == curses.KEY_RIGHT and col_num + 1 < 3:
+            game[row_num][col_num] = game[row_num][col_num][1]
+            col_num += 1
+            game[row_num][col_num] = "{" + game[row_num][col_num] +"}"
+        elif key == curses.KEY_LEFT and col_num > 0:
+            game[row_num][col_num] = game[row_num][col_num][1]
+            col_num -= 1
+            game[row_num][col_num] = "{" + game[row_num][col_num] +"}"
+        elif key == curses.KEY_DOWN and row_num + 1 < 3:
+            game[row_num][col_num] = game[row_num][col_num][1]
+            row_num += 1
+            game[row_num][col_num] = "{" + game[row_num][col_num] +"}"
+        elif key == curses.KEY_UP and row_num > 0:
+            game[row_num][col_num] = game[row_num][col_num][1]
+            row_num -= 1
+            game[row_num][col_num] = "{" + game[row_num][col_num] +"}"
+        if key == ord("\n"):
+            game[row_num][col_num] = players[rounds % 2]
+            rounds += 1
+            if col_num < 2:
+                col_num += 1
+            elif row_num < 2:
+                row_num += 1
+            else:
+                break
+            game[row_num][col_num] = "{" + game[row_num][col_num] +"}"
+            
+    curses.endwin()
+    return rounds
+        
+def prompt(game: list, players: dict):
+    print("The Current Game Grid:")
+    for row in game:
+        print(" ".join(row))
+    inputting = True
+    while inputting:
+        print("Player 1: " + players[0])
+        print("Player 2: " + players[1])
+        selection = (input("Are you Player 1 or 2? (1/2/s)"))
+        print("(input s if you want to switch the symbols for players)")
         if selection == "1":
             return players[0]
         elif selection == "2":
             return players[1]
+        elif selection == "s":
+            players[0], players[1] = players[1], players[0]
         else:
             print("Invalid input! Please input again.")
     
-def print_result():
-    with open("step.csv", newline="") as result_file:
-        reader = csv.reader(result_file, delimiter=" ")
-        for row in reader:
+def print_result(result, rounds):
+    if rounds < len(result):
+        print(f"Round {rounds}:")
+        for row in result[rounds]:
             print(" ".join(row))
-        
+    else:
+        sys.exit()
     
+          
 def init_file():
     stepfile = open("step.csv", "w")
     stepfile.close()
-    permufile = open("permutation.csv", "w")
+    permufile = open("move.csv", "w")
     permufile.close()
     
 def main():
-    time_start = round(time.time(), 8)
-    process = psutil.Process()
+    #time_start = round(time.time(), 8)
+    #process = psutil.Process()
     init_file()
     game = [
         ["_", "_", "_"],
@@ -129,44 +187,42 @@ def main():
         ["_", "_", "_"]
     ]
     
-    PLAYERS = {
+    players = {
         0: "O", # player 1
         1: "X" # player 2
     }
     
-    user = prompt(game, PLAYERS)
+    starting_rounds = curses.wrapper(input_gamegrid, game, players)
+    user = prompt(game, players)
+    result = backtrack(game[0:], players, starting_rounds, user)
+    index = 0 # this is to keep track of the index of result to be outputted
+    #Note: result stores steps from the ongoing round to end
     
-    rounds = 0
-    for row in game:
-        for element in row:
-            if element != "_":
-                rounds += 1
-
-    result = backtrack(game[0:], PLAYERS, rounds, user)
     if result:   
         with open("step.csv", "a", newline='') as file:
             writer = csv.writer(file, delimiter=" ")
             writer.writerow(f"You are Player 1 ({user})".split())
-            for i, gamegrid in enumerate(result):
-                writer.writerow(f"Round {i + rounds}:".split())
+            for i, gamegrid in enumerate(result, starting_rounds):
+                writer.writerow(f"Round {i}:".split())
                 for row in gamegrid:
                     writer.writerow(row)
                     
-        with open("permutation.csv", "a", newline='') as file:
+        with open("move.csv", "a", newline='') as file:
             writer = csv.writer(file, delimiter=" ")
-            for row in result[-1]:
+            index += 1
+            for row in result[index]:
                 writer.writerow(row)
             file.write("\n")
-        print_result()
+        print_result(result, index)
     else:
-        print("Unwinable in this situation!")
+        print("No 100% win rate move in this situation!")
         
     checking = True
     while checking:
         cont = input("Continue to check solutions? (y/n)")
         if cont == "y":
-            checking = False
-            main()
+            index += 1
+            print_result(result, index)
         elif cont == "n":
             checking = False
             sys.exit()
